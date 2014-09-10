150506b66a664ec9e187c41a8a991529823034a4
wyj <wyj@abbcc.net>
2014-09-10 18:36:36 +0800

    14.9.10

--- app/scripts/utils/extend.js
+++ app/scripts/utils/extend.js
@@ -1,60 +1,43 @@
-// This example contributed by Dave Woldrich -- thanks!
-
-/* Simple JavaScript Inheritance
- * By John Resig http://ejohn.org/
- * MIT Licensed.
+/**
+ * @description 对象继承
+ * @namespace extend
+ * @author yongjin on 2014/9/9
  */
-// Inspired by base2 and Prototype
 (function(){
     var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-    // The base Class implementation (does nothing)
     this.Class = function(){};
-
-    // Create a new Class that inherits from this class
     Class.extend = function(prop) {
         var _super = this.prototype;
-
-        // Instantiate a base class (but only create the instance,
-        // don't run the init constructor)
         initializing = true;
         var prototype = new this();
         initializing = false;
-        // Copy the properties over onto the new prototype
+        // 原型属性继承
         for (var name in prop) {
-            // Check if we're overwriting an existing function
+            // 检测是否覆盖已经存在的方法
             prototype[name] = typeof prop[name] == "function" &&
                 typeof _super[name] == "function" && fnTest.test(prop[name]) ?
                 (function(name, fn){
                     return function() {
                         var tmp = this._super;
-                        // Add a new ._super() method that is the same method
-                        // but on the super-class
+                        // 新建一个超级父类
                         this._super = _super[name];
-                        // The method only need to be bound temporarily, so we
-                        // remove it when we're done executing
                         var ret = fn.apply(this, arguments);
                         this._super = tmp;
-
                         return ret;
                     };
                 })(name, prop[name]) :
                 prop[name];
         }
-
-        // The dummy class constructor
+        // 虚拟类构造函数
         function Class() {
-            // All construction is actually done in the init method
+            // 在init方法中执行construction初始化
             if ( !initializing && this.init )
                 this.init.apply(this, arguments);
         }
-
-        // Populate our constructed prototype object
         Class.prototype = prototype;
-
-        // Enforce the constructor to be what we expect
+        // 执行构造函数
         Class.prototype.constructor = Class;
-
-        // And make this class extendable
+        // 并使这个类可扩展
         Class.extend = arguments.callee;
 
         return Class;
